c8f66b8e0e9bae22695b8124f0d8474c484a80be
iMamad <mamad.frhi@gmail.com>
2020-10-03 15:05:03 +0330

    - Implemented Coordinator Design Pattern

---
+++ ARKit-CubiCasa/App/Coordinator/MainCoordinator.swift
@@ -0,0 +1,29 @@
+//
+//  MainCoordinator.swift
+//  iOS-Challenge
+//
+//  Created by iMamad on 6/3/20.
+//  Copyright © 2020 Farshad Mousalou. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class MainCoordinator: Coordinator {
+    var childCoordinators = [Coordinator]()
+    var navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+        navigationController.navigationBar.barTintColor = .systemBlue
+        navigationController.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navigationController.navigationBar.tintColor = .white
+    }
+    
+    func start() {
+        let mainVC = MainController()
+        navigationController.pushViewController(mainVC, animated: true)
+    }
+}
+
+
