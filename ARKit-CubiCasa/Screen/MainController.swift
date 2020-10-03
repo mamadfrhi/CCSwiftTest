d40a6c895dffcbf8d5a322f7939d11edbefa8ac2
iMamad <mamad.frhi@gmail.com>
2020-10-03 12:38:01 +0330

    - Added download function and StateManagement to the MainController

--- ARKit-CubiCasa/Screen/MainController.swift
+++ ARKit-CubiCasa/Screen/MainController.swift
@@ -11,13 +11,65 @@ import RealityKit
 
 class MainController: UIViewController {
     
-    @IBOutlet var arView: ARView!
+    //---------------------
+    // MARK: State Management
+    // Observer Design Pattern
+    // TODO: Do it with RxSwift OR Move the logic to the another class
+    //---------------------
+    private enum State {
+        case initial
+        case objectReady
+    }
     
-    override func viewDidLoad() {
-        Network().loadModel(object3D: .wateringCan) {
-            (result) in
-            print(result)
+    private var state: State = .initial {
+        didSet {
+            switch state {
+            case .objectReady:
+                print("Show the drop button")
+            case .initial:
+                print("Wait please")
+            }
         }
     }
     
+    
+    
+    
+    
+    //---------------------
+    // MARK: Variables
+    //---------------------
+    @IBOutlet var arView: ARView!
+    
+    private let network = Network()
+    private var objectToAdd: ModelEntity? = nil
+    
+    
+    //---------------------
+    // MARK: LifeCycle
+    //---------------------
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadObject()
+        //        arView.session.delegate = self
+    }
+    
+    //---------------------
+    // MARK: Functionalities
+    //---------------------
+    private func loadObject() {
+        network.loadModel(object3D: .wateringCan) {
+            [weak self]
+            (result) in
+            guard let sSelf = self else { return}
+            switch result {
+            case .success(let downloadedObject):
+                print("Model downloaded")
+                sSelf.objectToAdd = downloadedObject
+                sSelf.state = .objectReady
+            case .failure(let error):
+                print("Error loading model: \(error.localizedDescription)")
+            }
+        }
+    }
 }
