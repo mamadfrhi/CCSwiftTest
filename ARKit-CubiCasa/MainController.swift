2f5487c1d9c9d78f873aed06c8240b37e2b94f55
iMamad <mamad.frhi@gmail.com>
2020-10-03 10:41:21 +0330

    - Cleaned Xcode initial template

---
+++ ARKit-CubiCasa/MainController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ARKit-CubiCasa
+//
+//  Created by iMamad on 10/3/20.
+//  Copyright © 2020 Kabok. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class MainController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+}
