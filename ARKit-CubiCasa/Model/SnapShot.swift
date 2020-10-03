6dfe256eeef633160709e71e8ad270a61fbaaa02
iMamad <mamad.frhi@gmail.com>
2020-10-03 19:15:09 +0330

    - Implemented take snapshot function

---
+++ ARKit-CubiCasa/Model/SnapShot.swift
@@ -0,0 +1,15 @@
+//
+//  SnapShot.swift
+//  ARKit-CubiCasa
+//
+//  Created by iMamad on 10/3/20.
+//  Copyright © 2020 Kabok. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+struct SnapShot {
+    let image: UIImage
+    let cameraTransform: Transform
+}
