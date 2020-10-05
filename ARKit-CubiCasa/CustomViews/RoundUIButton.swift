ed611d130551da2b462168389f3e395648d1b4e8
iMamad <mamad.frhi@gmail.com>
2020-10-05 17:32:58 +0330

    - Charming UIButtons

--- ARKit-CubiCasa/CustomViews/RoundUIButton.swift
+++ ARKit-CubiCasa/CustomViews/RoundUIButton.swift
@@ -9,30 +9,43 @@
 import UIKit
 
 class RoundUIButton: UIButton {
+    
     override func layoutSubviews() {
         super.layoutSubviews()
-        // Round it
-        self.clipsToBounds = true
-        self.layer.cornerRadius = self.layer.frame.size.width/2;
-        
-        // Coloring
-        self.backgroundColor = .white
-        self.tintColor = .black
-        
-        // Adjust icon
+        adjustIcon()
+        colorIt()
+        border()
+        roundIt()
+        addAnimation()
+    }
+    
+    private func adjustIcon() {
         self.contentHorizontalAlignment = .fill
         self.contentVerticalAlignment = .fill
         
         // Inset for icon
         self.imageEdgeInsets = UIEdgeInsets(top: 25, left: 25, bottom: 25, right: 25)
-        
-        addAnimation()
     }
+    
+    private func colorIt() {
+        self.backgroundColor = .white
+        self.tintColor = .black
+    }
+    private func border() {
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.black.cgColor
+    }
+    private func roundIt() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = self.layer.frame.size.width/2;
+    }
+    
+    
     
     private func addAnimation() {
         let pulse = CASpringAnimation(keyPath: "transform.scale")
         pulse.duration = 0.4
-        pulse.fromValue = 0.98
+        pulse.fromValue = 0.97
         pulse.toValue = 1.0
         pulse.autoreverses = true
         pulse.repeatCount = .infinity
--- ARKit-CubiCasa/Screen/Main/ARController.swift
+++ ARKit-CubiCasa/Screen/Main/ARController.swift
@@ -62,6 +62,7 @@ class ARController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: true)
+        
     }
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
